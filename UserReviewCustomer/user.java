b943c3bc28f1f7b2c420e01efda175d125c3ad78
Nigel Cheong <103913291+ncheong004@users.noreply.github.com>
2022-11-05 17:41:16 +0800

    Update user.java

--- UserReviewCustomer/user.java
+++ UserReviewCustomer/user.java
@@ -1,29 +1,61 @@
 import java.util.Scanner;
 class User extends Person{
-  public User(String firstName, String lastName) {
-		super(firstName, lastName);
-		// TODO Auto-generated constructor stub
-	}
   private String password;
   private userType userType;
-  /*public void setPassword(){
+  public void setPassword(){
     Scanner sc = new Scanner(System.in);
     System.out.print("New Password: ");
-    String pw = sc.nextLine();
+    String pw = sc.next();
     boolean valid = false;
+    char c; int i;
+    boolean hasLowerCase = false, hasUpperCase = false, hasNumber = false;
     while (!valid){
-      
+      //check length
+      valid = true; //initialise as true before checking
+      if (pw.length()<8) {
+        valid = false;
+        System.out.println("Password requires at least 8 characters");
+      }
+      //check for uppercase, lowercase, number
+      for (i=0;i<pw.length();i++){
+        c = pw.charAt(i);
+        if (Character.isLowerCase(c))
+          hasLowerCase = true;
+        else if (Character.isUpperCase(c))
+          hasUpperCase = true;
+        else if (Character.isDigit(c))
+          hasNumber = true;
+      }
+      if (!hasLowerCase){
+        valid = false; 
+        System.out.println("Password requires at least 1 lower case");
+      }
+      if (!hasUpperCase){
+        valid = false; 
+        System.out.println("Password requires at least 1 upper case");
+      }
+      if (!hasNumber){
+        valid = false; 
+        System.out.println("Password requires at least 1 number");
+      }
+      //check for special characters
+      if (!(pw.contains("@") || pw.contains("#") || pw.contains("!") || pw.contains("~") || pw.contains("$") || pw.contains("%") || pw.contains("^") || pw.contains("&") || pw.contains("*") || pw.contains("(") || pw.contains(")") || pw.contains("-") || pw.contains("+") || pw.contains("/")|| pw.contains(":") || pw.contains(".") || pw.contains(", ") || pw.contains("<") || pw.contains(">") || pw.contains("?")|| pw.contains("|"))) {
+        valid = false;
+        System.out.println("Password requires at least 1 special character");
+      }
+      if (!valid){
+        System.out.print("New password: ");
+        pw = sc.next();
+      }
     }
-    sc.close();
-  }*/
+    this.password = pw;
+  }
   public void setUserType(){
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner();
     System.out.print("Are you a customer? ");
     String response = sc.nextLine();
     response=response.toLowerCase();
-    if(response.equals("yes")) {this.userType=userType.CUSTOMER;System.out.println("Welcome to our booking system!");}
-    else this.userType=userType.STAFF;
-    sc.close();
+    if(response''==)
   }
   public userType getUserType(){
     return userType;
